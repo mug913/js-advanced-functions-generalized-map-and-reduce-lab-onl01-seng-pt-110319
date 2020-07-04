c86e0ce4be7b99441425685914fad2a6d198255b
Mug <brad.alexander913@gmail.com>
2020-07-03 17:05:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(arr, callback){
+    const result = []
+    for (let i = 0; i < arr.length; i++){
+        result[i] = callback(arr[i])
+    }
+return result
+}
+
+function reduce(arr, callback,start){
+    let result = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+    for (; i < arr.length; i++){
+       result = callback(arr[i], result)
+    }
+return result
+}
